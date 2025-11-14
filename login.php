d8b425dd1119bdb8b39f2b0785e64db4764b79bc
oreva orior <oreva.orior@datasphir.com>
2025-11-14 20:24:16 +0100

    make the header

--- login.php
+++ login.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Hems</title>
 
-    <link rel="stylesheet" href="./css/index.css">
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -25,6 +23,8 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,400,0,0" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <body class="h-screen overflow-auto  xl:bg-[#fafafa]" >
 
